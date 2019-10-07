47fd9c78dcc675966b74a27c0a036d52c0c9e426
VD-15 <valkyriedev15@gmail.com>
2019-10-07 17:27:48 +0100

    Eclipse project setup

---
+++ src/game/Program.java
@@ -0,0 +1,13 @@
+package game;
+
+public class Program
+{
+	/**
+	 * Entry point for the program
+	 * @param args Arguments for the program
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
