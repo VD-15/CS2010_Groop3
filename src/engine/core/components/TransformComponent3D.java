774ec09c4505988212e14dc42da5f4d0006c47d4
VD-15 <valkyriedev15@gmail.com>
2019-10-18 17:03:54 +0100

    Added JOML in place of propietary libraries

--- src/engine/core/components/TransformComponent3D.java
+++ src/engine/core/components/TransformComponent3D.java
@@ -1,18 +1,24 @@
 package engine.core.components;
 
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
+
 import engine.core.Component;
 import engine.core.Entity;
-import engine.util.Vector3;
 
 public class TransformComponent3D extends Component<TransformComponent3D>
 {
-	public Vector3 location;
-	public Quaternion rotation;
-	public Vector3 scale;
+	public Vector3f location;
+	public Quaternionf rotation;
+	public Vector3f scale;
 	
 	public TransformComponent3D(Entity parent)
 	{
 		super(TransformComponent3D.class, parent);
+		
+		this.location = new Vector3f();
+		this.rotation = new Quaternionf();
+		this.scale = new Vector3f(1f);
 	}
 	
 }
