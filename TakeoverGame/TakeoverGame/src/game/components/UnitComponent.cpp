05c9e3cab10bd7f3ca6e2ce06b082862a55d3dd3
Iqbal <49814727+arkandar@users.noreply.github.com>
2019-12-06 15:57:20 +0000

    added combat events

--- TakeoverGame/TakeoverGame/src/game/components/UnitComponent.cpp
+++ TakeoverGame/TakeoverGame/src/game/components/UnitComponent.cpp
@@ -6,26 +6,46 @@ namespace
 {
 	//have a system to check if units are in range
 	void OnLateUpdate(LateUpdateEvent& ev){
+
 		UnitComponent::ForEach([](UnitComponent* c)
 		{
-			
+			//check if unit is off cooldown
+			if (this->cooldown <= 0){
+				//check if unit is an enemy
+				if (this->team->team != c->team->team){
+					//get distance between enemy and this unit
+					Float distance = Vector2::Magnitude(c->transform->location - this->transform->location);
+
+					//check if the enemy is in aggro range
+					if ((distance <= this->aggro)){
+						//damage the enemy unit
+						c->DamageEnemy(this->damage);
+						//start the cooldown at the unit's attack speed
+						this->cooldown = this->atkspd;
+					}
+				}
+			}
 		});
-	
+	void OnUpdate(UpdateEvent& ev){
+		this->cooldown -= VLKTime::DeltaTime<Float>();
+	}
 }
 
 void CombatSystem::Init()
 {
-	EventBus<DamagedEvent>::Get().AddEventListener(DamagedEvent);
+	EventBus<LateUpdateEvent>::Get().AddEventListener(OnLateUpdate);
+	EventBus<UpdateEvent>::Get().AddEventListener(OnUpdate);
 }
 
 void CombatSystem::Destroy()
 {
-	EventBus<DamagedEvent>::Get().RemoveEventListener(DamagedEvent);
+	EventBus<LateUpdateEvent>::Get().RemoveEventListener(OnLateUpdate);
+	EventBus<UpdateEvent>::Get().RemoveEventListener(OnUpdate);
 }
 
 UnitComponent::UnitComponent(IEntity* e, const ProtoUnit* p, Team t):
-							Component<UnitComponent>(e)
-							{
+							Component<UnitComponent>(e){
+	this->transform = CreateComponent<TransformComponent3D>();
 	this->team = t;
 	this->health = p->health;
 	this->damage = p->damage;
@@ -33,15 +53,19 @@ UnitComponent::UnitComponent(IEntity* e, const ProtoUnit* p, Team t):
 	this->atkspd = p->atkspd;
 	this->speed = p->speed;
 	this->aggro = p->aggro;
-	this->foundEnemy = false;
+	this->cooldown = 0;
 	}
 	
 
-DamagedEvent::DamagedEvent(double damage){
+UnitComponent::DamageEnemy(double damage){
 	this->health -= damage;
+	if (this->health <= 0){
+		this->GetParent()->OnDelete();
+	}
 }
+/* not used in mvp
 void UnitComponent::UpgadeUnit(){
 	this->health *= 1.5;
 	this->damage *= 1.5;
 	this->tier +=1;
-}
+} */
--- TakeoverGame/TakeoverGame/src/game/components/UnitComponent.h
+++ TakeoverGame/TakeoverGame/src/game/components/UnitComponent.h
@@ -7,27 +7,19 @@ namespace tkv {
 		void Destroy();
 	}
 
-	//sends the damage value of the unit to the unit that is being attacked
-	struct AttackEvent : public Event{
-		AttackEvent(IEntity* e, double damage);
-		IEntity* target;
-	}
 
-	//decreased the health of the unit by the damage dealt to it
-	struct DamagedEvent : public Event{
-		DamagedEvent(double d);
-	}
 
 	struct UnitComponent : public Component<UnitComponent>
 	{
 		UnitComponent(IEntity* e, const ProtoUnit* p, Team t);
-		void UpgradeUnit();
-		// CheckForEnemies() maybe?
+		DamageEnemy(double damage);
+		//void UpgradeUnit(); not used in mvp
 
-		int tier = 1;
+		//int tier = 1;
 		double health, damage, range, atkspd, speed, aggro;
 		TeamComponent* team;
-		bool foundEnemy;
+		TransformComponent3D* transform;
+		double cooldown;
 	};
 
 }
